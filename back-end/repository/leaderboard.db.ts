ac050c7f3620d89713d03978a61dd2b5ff994aae
Manny <manny.singh@student.ucll.be>
2024-10-30 22:38:54 +0100

    Added fake profiles and users + modified leaderboard. Changed front end leaderboard page so it has 3 attributes (excl rank number)

--- back-end/repository/leaderboard.db.ts
+++ back-end/repository/leaderboard.db.ts
@@ -5,15 +5,27 @@ import { Profile } from '../model/profile';
 const leaderboards = [
     new Leaderboard({
         id: 1,
-        rankings: Array.from({ length: 5 }, (_, i) => profileDb.getProfileById(i + 1)).filter(user => user !== null) as Profile[],
-        maxPlayers: 5,
+        rankings: Array.from({ length: 10 }, (_, i) => profileDb.getProfileById(i + 1)).filter(
+            (user) => user !== null
+        ) as Profile[],
+        maxPlayers: 10,
         type: 15,
     }),
     new Leaderboard({
         id: 2,
-        rankings: Array.from({ length: 5 }, (_, i) => profileDb.getProfileById(i + 1)).filter(user => user !== null) as Profile[],
-        maxPlayers: 5,
+        rankings: Array.from({ length: 10 }, (_, i) => profileDb.getProfileById(i + 11)).filter(
+            (user) => user !== null
+        ) as Profile[],
+        maxPlayers: 10,
         type: 30,
+    }),
+    new Leaderboard({
+        id: 3,
+        rankings: Array.from({ length: 10 }, (_, i) => profileDb.getProfileById(i + 21)).filter(
+            (user) => user !== null
+        ) as Profile[],
+        maxPlayers: 10,
+        type: 60,
     }),
 ];
 
