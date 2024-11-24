182c0e31c3155c2d58e495bfcac395925ad09bfe
Manny <manny.singh@student.ucll.be>
2024-11-24 12:05:49 +0100

    Tried to fix tests

--- back-end/test/model/game.test.ts
+++ back-end/test/model/game.test.ts
@@ -1,50 +1,55 @@
-// import { set } from 'date-fns';
-// import { Game } from '../../model/game';
-// import { User } from '../../model/user';
-// import { Role } from '../../types';
+import { set } from 'date-fns';
+import { Game } from '../../model/game';
+import { User } from '../../model/user';
+import { Role } from '../../types';
 
-// const user1 = new User({
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     email: 'john.doe@ucll.be',
-//     password: 'johnd123',
-//     role: Role.Player,
-// });
+const user1 = new User({
+    id: 1,
+    username: 'JohnDoe',
+    email: 'john.doe@ucll.be',
+    password: 'johnd123',
+    role: Role.Player,
+    creationDate: new Date(),
+});
 
-// const user2 = new User({
-//     firstName: 'Jane',
-//     lastName: 'Toe',
-//     email: 'jane.toe@ucll.be',
-//     password: 'janet123',
-//     role: Role.Admin,
-// });
+const user2 = new User({
+    id: 2,
+    username: 'JaneToe',
+    email: 'jane.toe@ucll.be',
+    password: 'janet123',
+    role: Role.Admin,
+    creationDate: new Date(),
+});
 
-// const startDate = set(new Date(), { hours: 8, minutes: 30 });
-// const endDate = set(new Date(), { hours: 10, minutes: 30 });
+const startDate = set(new Date(), { hours: 8, minutes: 30 });
+const endDate = set(new Date(), { hours: 10, minutes: 30 });
 
-// test('given: missing start date, when: game is created, then: an error is thrown', () => {
-//     // given
-//     const invalidGame = {
-//         startDate: null,
-//         endDate: endDate,
-//         players: [user1, user2],
-//         status: 'active',
-//     };
+test('given: missing start date, when: game is created, then: an error is thrown', () => {
+    // given
+    // const invalidGame = {
+    //     startDate: null as any,
+    //     endDate: endDate,
+    //     users: [user1, user2],
+    // };
 
-//     // when
-//     const game = () => new Game(invalidGame);
+    // when
+    const game = () =>
+        new Game({
+            startDate: null,
+            endDate: endDate,
+            users: [user1, user2],
+        });
 
-//     // then
-//     expect(game).toThrow('Start date is required');
-// });
+    // then
+    expect(game).toThrow('Start date is require');
+});
 
 // test('given: missing end date, when: game is created, then: an error is thrown', () => {
 //     // given
 //     const invalidGame = {
 //         startDate: startDate,
 //         endDate: null,
-//         players: [user1, user2],
-//         status: 'active',
+//         users: [user1, user2],
 //     };
 
 //     // when
@@ -54,13 +59,12 @@
 //     expect(game).toThrow('End date is required');
 // });
 
-// test('given: start date is after end date, when: game is created, then: an error is thrown', () => {
+// test('given: start date after end date, when: game is created, then: an error is thrown', () => {
 //     // given
 //     const invalidGame = {
-//         startDate: startDate,
-//         endDate: set(new Date(), { hours: 7, minutes: 30 }),
-//         players: [user1, user2],
-//         status: 'active',
+//         startDate: endDate,
+//         endDate: startDate,
+//         users: [user1, user2],
 //     };
 
 //     // when
@@ -75,8 +79,7 @@
 //     const invalidGame = {
 //         startDate: startDate,
 //         endDate: endDate,
-//         players: [],
-//         status: 'active',
+//         users: [],
 //     };
 
 //     // when
@@ -86,37 +89,21 @@
 //     expect(game).toThrow('At least one player is required');
 // });
 
-// test('given: missing status, when: game is created, then: an error is thrown', () => {
-//     // given
-//     const invalidGame = {
-//         startDate: startDate,
-//         endDate: endDate,
-//         players: [user1, user2],
-//         status: '',
-//     };
+test('given: valid data, when: game is created, then: game is created successfully', () => {
+    // given
+    const validGame = {
+        startDate: startDate,
+        endDate: endDate,
+        users: [user1, user2],
+    };
 
-//     // when
-//     const game = () => new Game(invalidGame);
+    // when
+    const game = new Game(validGame);
 
-//     // then
-//     expect(game).toThrow('Status is required');
-// });
-
-// test('given: valid values for game, when: game is created, then: game is created with those values', () => {
-//     // given
-//     const validGame = {
-//         startDate: startDate,
-//         endDate: endDate,
-//         players: [user1, user2],
-//         status: 'active',
-//     };
-
-//     // when
-//     const game = new Game(validGame);
-
-//     // then
-//     expect(game.getStartDate()).toEqual(validGame.startDate);
-//     expect(game.getEndDate()).toEqual(validGame.endDate);
-//     expect(game.getPlayers()).toEqual(validGame.players);
-//     expect(game.getStatus()).toBe(validGame.status);
-// });
+    // then
+    expect(game.getStartDate()).toEqual(startDate);
+    expect(game.getEndDate()).toEqual(endDate);
+    expect(game.getUsers().length).toBe(2);
+    expect(game.getUsers()[0].getUsername()).toBe('JohnDoe');
+    expect(game.getUsers()[1].getUsername()).toBe('JaneToe');
+});
