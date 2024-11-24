f67d2f2e5ff52305c81a61aea03949c68dc6baaf
Manny <manny.singh@student.ucll.be>
2024-11-24 12:36:32 +0100

    added leaderboardinput, typingtestinput, gameinput

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -2,11 +2,36 @@ export enum Role {
     Player = 'player',
     Admin = 'admin',
 }
+
 export type UserInput = {
     id?: number;
-    username: string;
-    email: string;
-    password: string;
-    creationDate: Date;
-    role: Role | null;
+    username?: string;
+    email?: string;
+    password?: string;
+    creationDate?: Date;
+    role?: Role;
+};
+
+export type TypingTestInput = {
+    id?: number;
+    wpm?: number;
+    accuracy?: number;
+    time?: number;
+    type?: string;
+    userId?: number;
+    gameId?: number;
+};
+
+export type LeaderboardInput = {
+    id?: number;
+    maxPlayers?: number;
+    type?: number;
+    scores?: TypingTestInput[];
+};
+
+export type GameInput = {
+    id?: number;
+    startDate?: Date;
+    endDate?: Date;
+    users?: UserInput[];
 };
