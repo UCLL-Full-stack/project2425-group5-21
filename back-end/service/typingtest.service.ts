ee48301cbcc05f5aa9a7974e6d4b92318430785b
Manny <manny.singh@student.ucll.be>
2024-11-28 20:42:27 +0100

    Implemented getTypingTest by roles (doesnt work yet)

--- back-end/service/typingtest.service.ts
+++ back-end/service/typingtest.service.ts
@@ -1,8 +1,27 @@
+import { UnauthorizedError } from 'express-jwt';
 import { TypingTest } from '../model/typingTest';
 import typingtestDb from '../repository/typingtest.db';
+
+const getTypingTest = async ({
+    username,
+    role,
+}: {
+    username: string;
+    role: string;
+}): Promise<TypingTest[]> => {
+    if (role === 'admin') {
+        return typingtestDb.getAllTypingTests();
+    } else if (role === 'player') {
+        return typingtestDb.getTypingTestsByUsername(username);
+    } else {
+        throw new UnauthorizedError('credentials_required', {
+            message: 'You are not authorized to access this resource.',
+        });
+    }
+};
 
 const getAllTypingTests = async (): Promise<TypingTest[]> => {
     return typingtestDb.getAllTypingTests();
 };
 
-export default { getAllTypingTests };
+export default { getAllTypingTests, getTypingTest };
