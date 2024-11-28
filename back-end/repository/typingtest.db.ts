d3e3a8c9722da60702ef462be825a0d1e9733951
Manny <manny.singh@student.ucll.be>
2024-11-28 20:41:21 +0100

    Added a getTypingTestByUsername

--- back-end/repository/typingtest.db.ts
+++ back-end/repository/typingtest.db.ts
@@ -10,7 +10,23 @@ const getAllTypingTests = async (): Promise<TypingTest[]> => {
         throw new Error('Database error. See server log for details.');
     }
 };
+const getTypingTestsByUsername = async (username: string): Promise<TypingTest[]> => {
+    try {
+        const typingTestsPrisma = await database.typingTest.findMany({
+            where: {
+                user: {
+                    username,
+                },
+            },
+        });
+        return typingTestsPrisma.map((typingTestPrisma) => TypingTest.from(typingTestPrisma));
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error. See server log for details.');
+    }
+};
 
 export default {
     getAllTypingTests,
+    getTypingTestsByUsername,
 };
