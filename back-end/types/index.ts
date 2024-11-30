0e009a26bf0d1753ab842db31260924299cfc504
Manny <manny.singh@student.ucll.be>
2024-11-30 22:08:05 +0100

    Modified types backend

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -11,26 +11,26 @@ export type UserInput = {
 
 export type TypingTestInput = {
     id?: number;
-    wpm?: number;
-    accuracy?: number;
-    time?: number;
-    type?: string;
-    userId?: number;
+    wpm: number;
+    accuracy: number;
+    time: number;
+    type: string;
+    userId: number;
     gameId?: number;
 };
 
 export type LeaderboardInput = {
     id?: number;
-    maxPlayers?: number;
-    type?: number;
-    scores?: TypingTestInput[];
+    maxPlayers: number;
+    type: number;
+    scores: TypingTestInput[];
 };
 
 export type GameInput = {
     id?: number;
-    startDate?: Date;
-    endDate?: Date;
-    users?: UserInput[];
+    startDate: Date;
+    endDate: Date;
+    users: UserInput[];
 };
 
 export type AuthenticationResponse = {
