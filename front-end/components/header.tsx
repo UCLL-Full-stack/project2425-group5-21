cc84277de6d254e8442c9a684d158a8e6caf7c4e
Manny <manny.singh@student.ucll.be>
2024-12-01 20:30:18 +0100

    Changed header so that it adds usestates for loggedInUser, a logout function, Changed layout, added login and logout and when logged in username should apear

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -1,25 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { User } from "@/types";
 
 const Header: React.FC = () => {
-    return (
-        <header className="p-4 bg-[#120e17] border-b border-[#e8e6ea]/10 ">
-            <div className="container mx-auto flex items-center w-full">
-                <a className="text-2xl font-semibold text-white-50 -ml-4 text-[#e8e6ea]" href="#">
-                    MR Typer
-                </a>
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+  useEffect(() => {
+    const userString = localStorage.getItem("loggedInUser");
+    if (userString) {
+      const user: User = JSON.parse(userString);
+      setLoggedInUser(user);
+    }
+  }, []);
 
-                <nav className="flex space-x-6 ml-auto">
-                    <Link href="/" className="nav-link text-lg text-white hover:text-[#49a8b8]">
-                        Home
-                    </Link>
-                    <Link href="/leaderboards" className="nav-link text-lg text-white hover:text-[#49a8b8]">
-                        Leaderboard
-                    </Link>
-                </nav>
+  const handleClick = () => {
+    localStorage.removeItem("loggedInUser");
+    setLoggedInUser(null);
+  };
+
+  return (
+    <header className="p-4 bg-[#120e17] border-b border-[#e8e6ea]/10 ">
+      <div className="mx-auto flex">
+        <a
+          className="text-2xl font-semibold text-white-50 text-[#e8e6ea]"
+          href="#"
+        >
+          MR Typer
+        </a>
+
+        <nav className="flex space-x-6 ml-auto">
+          <Link
+            href="/"
+            className="px-4 text-white text-xl hover:bg-gray-600 rounded-lg"
+          >
+            Home
+          </Link>
+          <Link
+            href="/leaderboards"
+            className="px-4 text-white text-xl hover:bg-gray-600 rounded-lg"
+          >
+            Leaderboard
+          </Link>
+
+          {!loggedInUser && (
+            <Link
+              href="/login"
+              className="px-4 text-white text-xl hover:bg-gray-600 rounded-lg"
+            >
+              Login
+            </Link>
+          )}
+          {loggedInUser && (
+            <a
+              href="/login"
+              onClick={handleClick}
+              className="px-4 text-white text-xl hover:bg-gray-600 rounded-lg"
+            >
+              Logout
+            </a>
+          )}
+          {loggedInUser && (
+            <div className="px-4 text-white text-xl hover:bg-gray-600 rounded-lg">
+              Welcome, {loggedInUser.username}!
             </div>
-        </header>
-    );
+          )}
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
