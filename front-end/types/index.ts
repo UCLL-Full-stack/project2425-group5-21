291cd015cf318eda6b992bd0ace315db3ff1aa86
Manny <manny.singh@student.ucll.be>
2024-12-01 20:32:35 +0100

    Removed old types and added user type

--- front-end/types/index.ts
+++ front-end/types/index.ts
@@ -1,26 +1,15 @@
-type Role = 'admin' | 'player';
+export type Role = "admin" | "player" | "guest";
 
-export type Player = {
-    id: number;
-    username: string;
-    bio: string;
-    avgWPM: number;
-    highestWPM: number;
-    role: string;
+export type User = {
+  id?: number;
+  username?: string;
+  email?: string;
+  password?: string;
+  creationDate?: Date;
+  role?: string;
 };
 
-export type Leaderboard = {
-    id: number;
-    maxPlayers: number;
-    rankings: (Player | null)[];
-};
-
-export type Profile = {
-    id?: number;
-    username: string;
-    bio: string;
-    avgWPM: number | null;
-    highestWPM: number | null;
-    startDate: Date | null;
-    role: Role | null;
+export type StatusMessage = {
+  message: string;
+  type: "error" | "success";
 };
