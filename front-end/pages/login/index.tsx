a1a1fecc9df8278bb8288e6ccbbfe8a5941501c9
Manny <manny.singh@student.ucll.be>
2024-12-01 20:42:09 +0100

    Made a background color and changed the margin top

--- front-end/pages/login/index.tsx
+++ front-end/pages/login/index.tsx
@@ -6,11 +6,19 @@ const Login: React.FC = () => {
   return (
     <>
       <Head>
-        <title>User Signup</title>
+        <title>User Login</title>
+        <meta name="description" content="Login to start playing!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
-        <section className="p-6 min-h-screen flex flex-col items-center">
+      <main className="h-screen bg-[#120e17] flex flex-col justify-center items-center text-white relative">
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat bg-center z-0"
+          style={{ backgroundImage: "url(/path/to/your-background-image.png)" }}
+        />
+
+        <section className="p-6 min-h-screen flex flex-col items-center mt-20">
           <UserLoginForm />
         </section>
       </main>
