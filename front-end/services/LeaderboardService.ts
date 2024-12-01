c7697e31851a83746766b3ae18b7e4866e46ab78
Manny <manny.singh@student.ucll.be>
2024-12-01 20:32:07 +0100

    Commented leaderboardService in front end (still need to be made)

--- front-end/services/LeaderboardService.ts
+++ front-end/services/LeaderboardService.ts
@@ -1,26 +1,26 @@
-import { Profile } from "@/types";
+// import { Profile } from "@/types";
 
-const getAllLeaderboards = async () => {
-  return fetch(process.env.NEXT_PUBLIC_API_URL + "/leaderboards", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-    },
-  });
-};
+// const getAllLeaderboards = async () => {
+//   return fetch(process.env.NEXT_PUBLIC_API_URL + "/leaderboards", {
+//     method: "GET",
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   });
+// };
 
-const getLeaderboardByType = async (type: number) => {
-  return fetch(process.env.NEXT_PUBLIC_API_URL + `/leaderboards/${type}`, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-    },
-  });
-};
+// const getLeaderboardByType = async (type: number) => {
+//   return fetch(process.env.NEXT_PUBLIC_API_URL + `/leaderboards/${type}`, {
+//     method: "GET",
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   });
+// };
 
-const LeaderboardService = {
-  getAllLeaderboards,
-  getLeaderboardByType,
-};
+// const LeaderboardService = {
+//   getAllLeaderboards,
+//   getLeaderboardByType,
+// };
 
-export default LeaderboardService;
+// export default LeaderboardService;
