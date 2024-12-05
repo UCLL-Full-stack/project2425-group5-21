3950f83487c10ab6c34ccc78f3e36edbbee6c3c0
Manny <manny.singh@student.ucll.be>
2024-12-05 17:07:57 +0100

    Changed small things in leaderboard

--- back-end/model/leaderboard.ts
+++ back-end/model/leaderboard.ts
@@ -1,23 +1,25 @@
+import {
+    Leaderboard as LeaderboardPrisma,
+    TypingTest as TypingTestPrisma,
+    User as UserPrisma,
+} from '@prisma/client';
 import { TypingTest } from './typingTest';
-import { Leaderboard as LeaderboardPrisma, TypingTest as TypingTestPrisma } from '@prisma/client';
 
 export class Leaderboard {
     readonly id?: number;
-    readonly maxScores: number;
     readonly type: number;
+    readonly maxScores: number;
     readonly scores: TypingTest[];
 
     constructor(leaderboard: {
         id?: number;
-        maxScores: number;
         type: number;
+        maxScores: number;
         scores: TypingTest[];
     }) {
-        this.validate(leaderboard);
-
         this.id = leaderboard.id;
-        this.maxScores = leaderboard.maxScores;
         this.type = leaderboard.type;
+        this.maxScores = leaderboard.maxScores;
         this.scores = leaderboard.scores;
     }
 
@@ -25,12 +27,12 @@ export class Leaderboard {
         return this.id;
     }
 
-    getMaxScores(): number {
-        return this.maxScores;
-    }
-
     getType(): number {
         return this.type;
+    }
+
+    getMaxScores(): number {
+        return this.maxScores;
     }
 
     getScores(): TypingTest[] {
@@ -41,15 +43,12 @@ export class Leaderboard {
         if (!leaderboard.scores || leaderboard.scores.length === 0) {
             throw new Error('Scores must contain at least one typing test');
         }
-
         if (!leaderboard.maxScores || leaderboard.maxScores <= 0) {
             throw new Error('Max players must be a positive integer');
         }
-
-        if (!leaderboard.type) {
+        if (leaderboard.type === undefined || leaderboard.type === null) {
             throw new Error('Type is required');
         }
-
         if (![15, 30, 60].includes(leaderboard.type)) {
             throw new Error('Type must be either 15, 30, or 60');
         }
@@ -66,14 +65,14 @@ export class Leaderboard {
 
     static from({
         id,
-        maxScores,
         type,
+        maxScores,
         scores,
-    }: LeaderboardPrisma & { scores: TypingTestPrisma[] }): Leaderboard {
+    }: LeaderboardPrisma & { scores: (TypingTestPrisma & { user: UserPrisma })[] }): Leaderboard {
         return new Leaderboard({
             id,
-            maxScores,
             type,
+            maxScores,
             scores: scores.map((score) => TypingTest.from(score)),
         });
     }
