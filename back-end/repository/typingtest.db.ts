e0a8868f95bf7c3b20eacd10497537fe07922b9b
Manny <manny.singh@student.ucll.be>
2024-12-08 12:59:17 +0100

    added get typingtest by userid + get typingtest by userid and type in typingtest.db.ts

--- back-end/repository/typingtest.db.ts
+++ back-end/repository/typingtest.db.ts
@@ -43,8 +43,36 @@ const deleteTypingTestsByUserId = async (userId: number): Promise<void> => {
     }
 };
 
+const getTypingTestsByUser = async (userId: number): Promise<TypingTest[]> => {
+    try {
+        const typingTestsPrisma = await database.typingTest.findMany({
+            where: { userId },
+            include: { user: true },
+        });
+        return typingTestsPrisma.map((typingTestPrisma) => TypingTest.from(typingTestPrisma));
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error. See server log for details.');
+    }
+};
+
+const getTypingTestsByUserAndType = async (userId: number, type: string): Promise<TypingTest[]> => {
+    try {
+        const typingTestsPrisma = await database.typingTest.findMany({
+            where: { userId, type },
+            include: { user: true },
+        });
+        return typingTestsPrisma.map((typingTestPrisma) => TypingTest.from(typingTestPrisma));
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error. See server log for details.');
+    }
+};
+
 export default {
     getAllTypingTests,
     getTypingTestsByUsername,
+    getTypingTestsByUser,
+    getTypingTestsByUserAndType,
     deleteTypingTestsByUserId,
 };
