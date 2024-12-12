8a791401cc37b038a7e881d8c1b5ab830381dcf3
Manny <manny.singh@student.ucll.be>
2024-12-12 22:51:05 +0100

    added front end updateUsername code

--- front-end/pages/stats/index.tsx
+++ front-end/pages/stats/index.tsx
@@ -3,12 +3,18 @@ import Header from "@/components/header";
 import TypingTestService from "@/services/TypingTestService";
 import { useState, useEffect } from "react";
 import { TypingTest, User } from "@/types";
+import UserService from "@/services/UserService";
+import { useRouter } from "next/router";
 
 const Stats: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [typingTests, setTypingTests] = useState<TypingTest[]>([]);
   const [user, setUser] = useState<User | null>(null);
+  const [newUsername, setNewUsername] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [statusMessage, setStatusMessage] = useState<string | null>(null);
+  const router = useRouter();
 
   const getAllTypingTests = async () => {
     setError(null);
@@ -37,6 +43,44 @@ const Stats: React.FC = () => {
       );
     }
   }, []);
+
+  const handleUsernameChange = async () => {
+    if (newUsername.length < 3) {
+      setError("Username must be at least 3 characters long.");
+      return;
+    }
+
+    if (user && user.id !== undefined) {
+      try {
+        const response = await UserService.updateUsername(user.id, newUsername);
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(`${errorData.status}: ${errorData.message}`);
+        }
+
+        const updatedUser = { ...user, username: newUsername };
+        setUser(updatedUser);
+        localStorage.removeItem("loggedInUser");
+
+        setStatusMessage(
+          "Username has been updated. Redirecting to login page..."
+        );
+        setTimeout(() => {
+          router.push("/login");
+        }, 3000);
+      } catch (err: any) {
+        setError(err.message || "Failed to update username.");
+        setStatusMessage(null);
+      }
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewUsername(e.target.value);
+    setError(null);
+    setStatusMessage(null);
+  };
 
   const calculateAverages = (time: number) => {
     const filteredTests = typingTests.filter((test) => test.time === time);
@@ -118,8 +162,11 @@ const Stats: React.FC = () => {
                           minutes
                         </td>
                         <td className="py-6">
-                          <button className="bg-[#4a90e2] hover:bg-[#357ac9] text-white font-bold py-2 px-4 rounded">
-                            Update
+                          <button
+                            onClick={() => setIsEditing(true)}
+                            className="bg-[#4a90e2] hover:bg-[#357ac9] text-white font-bold py-2 px-4 rounded"
+                          >
+                            Change name
                           </button>
                         </td>
                       </tr>
@@ -128,6 +175,51 @@ const Stats: React.FC = () => {
                 </table>
               </div>
             </div>
+            {isEditing && (
+              <div className="fixed inset-0 bg-black bg-opacity-85 flex items-center justify-center z-50">
+                <div className="bg-[#2a2d40] p-7 rounded-lg shadow-lg text-white">
+                  <h2 className="text-2xl font-semibold mb-4">
+                    Change Username
+                  </h2>
+                  <input
+                    type="text"
+                    value={newUsername}
+                    onChange={handleInputChange}
+                    className="mb-4 p-2 border rounded w-full"
+                    placeholder="Enter new username"
+                  />
+                  <div className="flex justify-end space-x-4">
+                    <button
+                      onClick={() => setIsEditing(false)}
+                      className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      onClick={handleUsernameChange}
+                      className="bg-[#4a90e2] hover:bg-[#357ac9] text-white font-bold py-2 px-4 rounded"
+                    >
+                      Change name
+                    </button>
+                  </div>
+                  {!error && (
+                    <p className="mt-4 text-[12px] font-bold text-gray-400">
+                      Please log in again with your new username.
+                    </p>
+                  )}
+                  {error && (
+                    <p className="mt-2 text-[12px] font-bold text-red-500">
+                      {error}
+                    </p>
+                  )}
+                  {statusMessage && (
+                    <p className="mt-2 text-[12px] font-bold text-green-500">
+                      {statusMessage}
+                    </p>
+                  )}
+                </div>
+              </div>
+            )}
             <div className="w-11/12 max-w-8xl flex justify-between mb-8">
               <div className="w-1/2 mr-20">
                 <h2 className="text-left text-2xl font-semibold py-4 text-[#b0b3c8]">
