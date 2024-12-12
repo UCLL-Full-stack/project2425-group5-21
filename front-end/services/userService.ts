d2b82f23803106b008effe2c195b56acc253304f
Manny <manny.singh@student.ucll.be>
2024-12-12 23:27:04 +0100

    added deleteUser in user service front end

--- front-end/services/userService.ts
+++ front-end/services/userService.ts
@@ -33,10 +33,23 @@ const updateUsername = (userId: number, newUsername: string) => {
   });
 };
 
+const deleteUser = (userId: number) => {
+  const userString = localStorage.getItem("loggedInUser");
+  const token = userString ? JSON.parse(userString).token : null;
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${userId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
 const UserService = {
   loginUser,
   registerUser,
   updateUsername,
+  deleteUser,
 };
 
 export default UserService;
