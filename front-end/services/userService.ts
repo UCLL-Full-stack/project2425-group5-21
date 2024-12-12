c5ba4162007a12d2dd19161b0a4442d0c17e2cc8
Manny <manny.singh@student.ucll.be>
2024-12-12 22:50:19 +0100

    added updateUsername in userservice front end

--- front-end/services/userService.ts
+++ front-end/services/userService.ts
@@ -20,9 +20,23 @@ const registerUser = (user: User) => {
   });
 };
 
+const updateUsername = (userId: number, newUsername: string) => {
+  const userString = localStorage.getItem("loggedInUser");
+  const token = userString ? JSON.parse(userString).token : null;
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${userId}/username`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ username: newUsername }),
+  });
+};
+
 const UserService = {
   loginUser,
   registerUser,
+  updateUsername,
 };
 
 export default UserService;
