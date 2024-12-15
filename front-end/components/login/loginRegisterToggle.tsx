2e2f3a589505bcd3c61a11b122d89cff9c2cb8e0
Manny <manny.singh@student.ucll.be>
2024-12-15 23:04:43 +0100

    moved these components to diffrent folder

---
+++ front-end/components/login/loginRegisterToggle.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import UserLoginForm from "@/components/login/userLoginForm";
+import RegisterForm from "@/components/register/userRegisterForm";
+
+const LoginRegisterToggle: React.FC = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <div className="flex justify-center space-x-4 mb-4">
+        <button
+          onClick={() => setIsLogin(true)}
+          className={`py-2 px-10 rounded-md ${
+            isLogin ? "bg-blue-600 text-white" : "bg-gray-700 text-gray-300"
+          }`}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => setIsLogin(false)}
+          className={`py-2 px-10 rounded-md ${
+            !isLogin ? "bg-blue-600 text-white" : "bg-gray-700 text-gray-300"
+          }`}
+        >
+          Register
+        </button>
+      </div>
+      {isLogin ? <UserLoginForm /> : <RegisterForm />}
+    </div>
+  );
+};
+
+export default LoginRegisterToggle;
---
+++ front-end/components/login/userLoginForm.tsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import userService from "@/services/UserService";
+import { StatusMessage } from "@/types";
+
+const UserLoginForm: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [statusMessages, setStatusMessages] = useState<StatusMessage[]>([]);
+  const router = useRouter();
+
+  const clearErrors = () => {
+    setUsernameError(null);
+    setPasswordError(null);
+    setStatusMessages([]);
+  };
+
+  const validate = (): boolean => {
+    let result = true;
+
+    if (!username || username.trim() === "") {
+      setUsernameError("Username is required");
+      result = false;
+    } else if (username.length < 3 || username.length > 50) {
+      setUsernameError("The username must be between 3 and 50 characters.");
+    }
+
+    if (!password || password.trim() === "") {
+      setPasswordError("Password is required");
+      result = false;
+    } else if (password.length < 5) {
+      setPasswordError("The password must be at least 5 characters long.");
+      result = false;
+    }
+
+    return result;
+  };
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    clearErrors();
+
+    if (!validate()) {
+      return;
+    }
+
+    const user = { username, password };
+    const response = await userService.loginUser(user);
+
+    if (response.status === 200) {
+      const user = await response.json();
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify({
+          token: user.token,
+          username: user.username,
+          role: user.role,
+        })
+      );
+      setStatusMessages([
+        {
+          message: "Login succesful. Redirecting to homepage...",
+          type: "success",
+        },
+      ]);
+      setTimeout(() => {
+        router.push("/");
+      }, 2000);
+    } else {
+      const errorData = await response.json();
+
+      setStatusMessages([
+        {
+          message:
+            errorData.message ||
+            "An error has occurred. Please try again later.",
+          type: "error",
+        },
+      ]);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white rounded-md"
+          />
+          {usernameError && (
+            <p className="text-red-500 text-sm mt-1">{usernameError}</p>
+          )}
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white rounded-md"
+          />
+          {passwordError && (
+            <p className="text-red-500 text-sm mt-1">{passwordError}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-blue-600 text-white rounded-md"
+        >
+          Login
+        </button>
+      </form>
+      {statusMessages.map((status, index) => (
+        <div
+          key={index}
+          className={`mt-4 text-center text-lg font-semibold ${
+            status.type === "success" ? "text-green-500" : "text-red-500"
+          }`}
+        >
+          {status.message}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default UserLoginForm;
