29a5b4504497fb135c5c21b07216d49d7c68789b
Manny <manny.singh@student.ucll.be>
2024-12-16 21:59:32 +0100

    added language english and spanish to home page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Head from "next/head";
 import Header from "@/components/header";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import HomePage from "@/components/home/homePage";
 
 const Home: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
-        <title>MR Typer | Home</title>
+        <title>{t("app.title.home")}</title>
         <meta
           name="description"
           content="Select your game mode to start playing!"
@@ -17,11 +21,21 @@ const Home: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
+      <Header></Header>
 
       <HomePage />
     </>
   );
+};
+
+export const getServerSideProps = async (context: { locale: any }) => {
+  const { locale } = context;
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
+    },
+  };
 };
 
 export default Home;
