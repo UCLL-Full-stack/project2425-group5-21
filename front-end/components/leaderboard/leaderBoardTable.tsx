2e33a8fc7d49f73020e2289e3cae2c973ff26e37
Manny <manny.singh@student.ucll.be>
2024-12-16 23:25:01 +0100

    added language english and spanish to leaderboardTable component

--- front-end/components/leaderboard/leaderBoardTable.tsx
+++ front-end/components/leaderboard/leaderBoardTable.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import type { Leaderboard } from "@/types";
+import { useTranslation } from "next-i18next";
 
 const LeaderboardTable: React.FC<{ leaderboard: Leaderboard | null }> = ({
   leaderboard,
 }) => {
+  const { t } = useTranslation("common");
+
   return (
     <div className="w-11/12 max-w-7xl bg-[#2a2d40] rounded-lg shadow-lg overflow-hidden">
       <table className="w-full table-auto text-center text-lg">
         <thead>
           <tr className="bg-[#3b3f5c] text-[#b0b3c8]">
             <th className="py-6 font-semibold uppercase tracking-wider">
-              Rank
+              {t("leaderboard.rank")}
             </th>
             <th className="py-6 font-semibold uppercase tracking-wider">
-              Username
+              {t("leaderboard.username")}
             </th>
             <th className="py-6 font-semibold uppercase tracking-wider">WPM</th>
             <th className="py-6 font-semibold uppercase tracking-wider">
-              Accuracy
+              {t("leaderboard.accuracy")}
             </th>
           </tr>
         </thead>
