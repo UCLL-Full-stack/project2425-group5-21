5a48c7124572258cd5df13049eebf3043d70e940
Manny <manny.singh@student.ucll.be>
2024-12-16 23:25:27 +0100

    added language english and spanish to deleteUser component

--- front-end/components/stats/deleteUser.tsx
+++ front-end/components/stats/deleteUser.tsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import UserService from "@/services/UserService";
 import { useRouter } from "next/router";
-import { User } from "@/types";
+import { useTranslation } from "next-i18next";
 
 const DeleteUser: React.FC<{ userId: number }> = ({ userId }) => {
+  const { t } = useTranslation("common");
+
   const [statusMessage, setStatusMessage] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [user, setUser] = useState<User | null>(null);
   const [isRemoving, setIsRemoving] = useState<boolean>(false);
   const router = useRouter();
 
@@ -21,14 +22,13 @@ const DeleteUser: React.FC<{ userId: number }> = ({ userId }) => {
 
       localStorage.removeItem("loggedInUser");
 
-      setStatusMessage(
-        "User has been successfully deleted. Redirecting to login page..."
-      );
+      setStatusMessage(t("stats.profile.deleteUser.userDeleted"));
+
       setTimeout(() => {
         router.push("/login");
       }, 2000);
     } catch (err: any) {
-      setError(err.message || "Failed to delete user.");
+      setError(err.message || t("general.error"));
     }
   };
 
@@ -38,27 +38,29 @@ const DeleteUser: React.FC<{ userId: number }> = ({ userId }) => {
         onClick={() => setIsRemoving(true)}
         className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
       >
-        Delete
+        {t("stats.profile.deleteUser.delete")}
       </button>
       {isRemoving && (
         <div className="fixed inset-0 bg-black bg-opacity-85 flex items-center justify-center z-50">
           <div className="bg-[#2a2d40] p-6 rounded-lg shadow-lg text-white">
-            <h2 className="text-2xl font-semibold mb-4">Delete User</h2>
+            <h2 className="text-2xl font-semibold mb-4">
+              {t("stats.profile.deleteUser.deleteUser")}
+            </h2>
             <p className="mb-4">
-              Are you sure you want to delete your account?
+              {t("stats.profile.deleteUser.confirmDelete")}
             </p>
             <div className="flex justify-end space-x-4">
               <button
                 onClick={() => setIsRemoving(false)}
                 className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
               >
-                Cancel
+                {t("stats.profile.deleteUser.cancel")}
               </button>
               <button
                 onClick={() => handleDeleteUser()}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
               >
-                Delete
+                {t("stats.profile.deleteUser.delete")}
               </button>
             </div>
             {statusMessage && (
