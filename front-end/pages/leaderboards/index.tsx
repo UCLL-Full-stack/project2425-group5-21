d2a177b970f115a57eed80a0001afe6fbca4af35
Manny <manny.singh@student.ucll.be>
2024-12-16 21:59:42 +0100

    added language english and spanish to leaderboard page

--- front-end/pages/leaderboards/index.tsx
+++ front-end/pages/leaderboards/index.tsx
@@ -2,20 +2,34 @@ import Head from "next/head";
 import Header from "@/components/header";
 import type { Leaderboard } from "@/types";
 import LeaderboardPage from "@/components/leaderboard/leaderboardPage";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Leaderboard: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
-        <title>Leaderboard</title>
+        <title>{t("app.title.leaderboard")}</title>
         <meta name="description" content="check the top ranked players!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Header></Header>
       <LeaderboardPage />
     </>
   );
+};
+
+export const getServerSideProps = async (context: { locale: any }) => {
+  const { locale } = context;
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
+    },
+  };
 };
 
 export default Leaderboard;
