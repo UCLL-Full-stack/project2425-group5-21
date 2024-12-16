21986e5bf731d0109700b58d9fba000ab572c8fa
Manny <manny.singh@student.ucll.be>
2024-12-16 22:00:51 +0100

    added language english and spanish to stats page

--- front-end/pages/stats/index.tsx
+++ front-end/pages/stats/index.tsx
@@ -1,20 +1,34 @@
 import Head from "next/head";
 import Header from "@/components/header";
 import ProfileStats from "@/components/stats/profileStats";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Stats: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
-        <title>Stats</title>
+        <title>{t("app.title.stats")}</title>
         <meta name="description" content="check your stats!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Header></Header>
       <ProfileStats />
     </>
   );
+};
+
+export const getServerSideProps = async (context: { locale: any }) => {
+  const { locale } = context;
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
+    },
+  };
 };
 
 export default Stats;
