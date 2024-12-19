7a8fa8e062c153e57d281f16d25a2de541f27f63
Robert Callens <ByteGamerYT@gmail.com>
2024-12-19 03:15:05 +0100

    Typingtest Service Front end

--- front-end/services/TypingTestService.ts
+++ front-end/services/TypingTestService.ts
@@ -9,6 +9,29 @@ const getTypingTests = async () => {
     },
   });
 };
+interface TypingTestPayload {
+  wpm: number;
+  accuracy: number;
+  time: number;
+  type: string;
+}
+
+export const createTypingTest = async (payload: TypingTestPayload) => {
+  const userString = localStorage.getItem("loggedInUser");
+  const token = userString ? JSON.parse(userString).token : null;
+  console.log(token);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/typingtests`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+
+    },
+    body: JSON.stringify(payload),
+  });
+  return response.json();
+};
+
 
 const TypingTestService = {
   getTypingTests,
